3d14ef6b445e3f35982e75c0228ed0d958eb0a07
FND <fnd@oda.(none)>
2009-12-21 15:04:19 +0000

    util: clarified documentation

--- util.js
+++ util.js
@@ -8,7 +8,8 @@ $.fn.attach = function(html) {
 };
 
 // appends multiple elements generated from data array
-// generator function is passed data item and index, must return a jQuery object
+// generator function is invoked for each item; it is passed item and index as
+// arguments and must return jQuery object
 $.fn.multipend = function(items, generator) { // TODO: rename
 	return this.append($.map(items, function(item, i) {
 		return generator.apply(this, arguments)[0];
