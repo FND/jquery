cd107d68300db88bf4fce23331684002057870fd
FND <fnd@fnd-oda.localdomain>
2012-04-12 08:09:02 +0200

    mintree: added options handling
    avoiding excessive closure nesting required class-based refactoring in
    order to retain state

--- jquery.mintree/jquery.mintree.js
+++ jquery.mintree/jquery.mintree.js
@@ -5,31 +5,36 @@ jQuery.fn.mintree = (function($) {
 
 "use strict";
 
-var glyphs = { // TODO: configurable
-	open: "▹",
-	closed: "▿"
+function MinTree(root, options) {
+	this.options = $.extend({}, this.defaults, options);
+	var el = $(root).addClass("mintree"),
+		self = this;
+	$("li", root).each(function(i, node) {
+		self.injectToggle(node);
+	});
+	el.on("click", "a.toggle", this.onToggle);
+}
+MinTree.prototype = {
+	defaults: {
+		glyphs: {
+			open: "▹",
+			closed: "▿"
+		}
+	},
+	onToggle: function(ev) { // TODO: option to exclude individual lists
+		$(this).closest("li").children("ul, ol").toggle(); // XXX: use of `children` disallows wrappers
+	},
+	injectToggle: function(node) { // TODO: option to exclude individual lists
+		$('<a href="javascript:;" class="toggle" />').
+				text(this.options.glyphs.open).
+				prependTo(node);
+	}
 };
 
-var onToggle, injectToggle;
-
-var mintree = function(i, node) {
-	var el = $(node).addClass("mintree");
-	$("li", node).each(injectToggle);
-	el.on("click", "a.toggle", onToggle);
-};
-
-onToggle = function(ev) { // TODO: option to exclude individual lists
-	var desc = $(this).closest("li").children("ul, ol"); // XXX: use of `children` disallows wrappers
-	desc.toggle();
-};
-
-injectToggle = function(i, node) { // TODO: option to exclude individual lists
-	$('<a href="javascript:;" class="toggle" />').text(glyphs.open).
-			prependTo(node);
-};
-
-return function() {
-	this.each(mintree);
+return function(options) {
+	this.each(function(i, node) {
+		new MinTree(node, options);
+	});
 	return this;
 }; // TODO: expose internal functions?
 
